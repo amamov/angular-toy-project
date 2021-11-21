18bb986d455a8b287597ea3f8abf75bc5acc20c9
amamov <amamov@kakao.com>
2021-11-22 02:27:36 +0900

    apply lint

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 
+// 컴포넌트는 앱을 구성하는 기본 단위이다.
 @Component({
-  selector: 'app-root',
+  selector: 'app-root', // 템플릿에 사용될 CSS 셀렉터를 지정한다.
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'angular-toy-project';
