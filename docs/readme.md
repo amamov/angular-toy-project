03dc1d74f011ffed3b4222e5c542307584ecec8f
amamov <amamov@kakao.com>
2021-11-24 15:26:37 +0900

    life cycle

--- docs/readme.md
+++ docs/readme.md
@@ -4,4 +4,38 @@
 
 ## Dependency Injection
 
+> 도중에 해당 서비스에 대한 Providers 값이 있다면 검색 종료
+
+1. 자신의 컴포넌트
+
+2. 부모 컴포넌트
+
+3. 루트 컴포넌트까지 계속 부모-자식 관계를 타고 올라간다.
+
+4. 루트 모듈
+
+5. 하위 모듈
+
+6. 자신이 속한 모듈까지 계속 내려옴
+
 ## Directives
+
+---
+
+## Component Life Cycle
+
+1. `ngOnChanges`
+
+2. `ngOnInit`
+
+3. `ngDoCheck`
+
+4. `ngAfterContentInit`
+
+5. `ngAfterContentChecked`
+
+6. `ngAfterViewInit`
+
+7. `ngAfterViewChecked`
+
+8. `ngDestroy`
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: [],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-toy-project';
+
+  constructor() {
+    console.log('constructor');
+  }
+
+  ngOnInit(): void {
+    console.log('ngOnInit');
+  }
+  ngOnChanges(changes: SimpleChanges): void {
+    //Called before any other lifecycle hook. Use it to inject dependencies, but avoid any serious work here.
+    console.log('ngOnChanges');
+  }
+  ngDoCheck(): void {
+    //Called every time that the input properties of a component or a directive are checked. Use it to extend change detection by performing a custom check.
+    console.log('ngDoCheck');
+  }
+  ngAfterContentInit(): void {
+    //Called after ngOnInit when the component's or directive's content has been initialized.
+    console.log('ngAfterContentInit');
+  }
+  ngAfterContentChecked(): void {
+    //Called after every check of the component's or directive's content.
+    console.log('ngAfterContentChecked');
+  }
+  ngAfterViewInit(): void {
+    //Called after ngAfterContentInit when the component's view has been initialized. Applies to components only.
+    console.log('ngAfterViewInit');
+  }
+  ngAfterViewChecked(): void {
+    //Called after every check of the component's view. Applies to components only.
+    console.log('ngAfterViewChecked');
+  }
+  ngOnDestroy(): void {
+    //Called once, before the instance is destroyed.
+    console.log('ngOnDestroy');
+  }
 }
